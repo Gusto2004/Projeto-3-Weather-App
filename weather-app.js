1aee35a42eec12146599150982bba0a1d4167ea5
Gusto2004 <gusto.main1507@gmail.com>
2026-07-03 00:22:59 +0100

    push

--- weather-app.js
+++ weather-app.js
@@ -48,3 +48,16 @@ function obterIcone(descricao) {
 
   return "🌡️";
 }
+
+const botaoTema = document.querySelector("#toggle-tema");
+const body = document.body;
+
+botaoTema.addEventListener("click", function () {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    botaoTema.textContent = "☀️";
+  } else {
+    botaoTema.textContent = "🌙";
+  }
+});
